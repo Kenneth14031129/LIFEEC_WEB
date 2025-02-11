c5a01c083de8c45a18d670cb95f0cdd009f3c1cf
JustineKennethGaviola <kenneth.gaviola@rcccolabsolutions.com>
2025-02-11 20:16:30 +0800

    update server.js

--- backend/server.js
+++ backend/server.js
@@ -44,12 +44,12 @@ app.use('/api/emergency-alerts', emergencyAlertRoutes);
 app.use('/api', messageRoutes);
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, '../src/dist')));
+app.use(express.static(path.join(__dirname, '../dist')));
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../src/dist/index.html'));
+  res.sendFile(path.join(__dirname, '../dist/index.html'));
 });
 
 const PORT = process.env.PORT || 5000;
